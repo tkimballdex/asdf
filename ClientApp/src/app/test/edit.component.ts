35fd842ce90663bd43d9c71e305f7306677309de
Fineas Berindei <fberindei@dexwell.com>
2021-01-22 21:10:32 -0800

    test-type updated

--- ClientApp/src/app/test/edit.component.ts
+++ ClientApp/src/app/test/edit.component.ts
@@ -1,81 +1,81 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { GridComponent } from '@syncfusion/ej2-angular-grids';
 import { DialogUtility, Dialog } from '@syncfusion/ej2-popups';
+import { TabComponent } from '@syncfusion/ej2-angular-navigations';
 import { AppRepository } from "../shared/app.repository";
 import { PageComponent } from '../shared/page.component';
-import { SiteRepository } from './repository';
+import { TestTypeRepository } from './repository';
 
 @Component({
-  selector: 'app-edit',
-  templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.css']
+    selector: 'test-edit',
+    templateUrl: './edit.component.html',
+    styleUrls: ['./edit.component.scss']
 })
-export class EditComponent extends PageComponent implements OnInit {
-  constructor(private route: ActivatedRoute, private router: Router, private appRepository: AppRepository, private repository: SiteRepository) {
-    super();
-}
-
-public record: any;
-public deleteDialog: Dialog;
-
-async ngOnInit() {
-    this.showSpinner();
-    this.app = await this.appRepository.getData();
-    this.privileges = this.app.privileges.sites;
-
-    var id = this.route.snapshot.paramMap.get('id');
-    this.record = await this.repository.get(id);
-    this.hideSpinner();
-
-    if (id == null) {
-        this.record.customerId = this.route.snapshot.paramMap.get('customerId');
+export class TestEditComponent extends PageComponent implements OnInit {
+    constructor(private route: ActivatedRoute, private router: Router, private appRepository: AppRepository, private repository: TestTypeRepository) {
+        super();
     }
-}
 
-async save() {
-    var add = !this.record.id;
-    this.showSpinner();
-    this.record.tenantId = this.appRepository.tenantId;
-    var returnValue = await this.repository.save(this.record);
-    this.hideSpinner();
+    public record: any;
+    public deleteDialog: Dialog;
 
-    if (returnValue && returnValue.error) {
-        this.showErrorMessage(returnValue.description);
+    //-----------------------------------------------------------------------------------------
+    async ngOnInit() {       
+        this.showSpinner();
+        this.app = await this.appRepository.getData();
+        this.privileges = this.app.privileges.vendors;
+
+        var id = this.route.snapshot.paramMap.get('id');
+        this.record = await this.repository.get(id);
+        this.hideSpinner();        
     }
-    else {
-        var success = returnValue && returnValue.updated;
-        this.showSaveMessage(success);
+    //-----------------------------------------------------------------------------------------
+    async save() {
+        var add = !this.record.id;
+        this.showSpinner();
+        this.record.tenantId = this.appRepository.tenantId;
+        var returnValue = await this.repository.save(this.record);
+        this.hideSpinner();
 
-        if (success) {
-            this.record = returnValue;
+        if (returnValue && returnValue.error) {
+            this.showErrorMessage(returnValue.description);
         }
+        else {
+            var success = returnValue && returnValue.updated;
+            this.showSaveMessage(success);
 
-        if (success && add) {
-            setTimeout(() => this.router.navigate(['/site/edit', returnValue.id]), 1000);
+            if (success) {
+                this.record = returnValue;
+            }
+
+            if (success && add) {
+                setTimeout(() => this.router.navigate(['/vendor/edit', returnValue.id]), 1000);
+            }
         }
     }
-}
-
-delete() {
-    this.deleteDialog = DialogUtility.confirm({
-        title: 'Delete Site',
-        content: `Are you sure you want to delete this Site <b>${this.record.userName}</b>?`,
-        okButton: { click: this.deleteOK.bind(this) }
-    });
-}
-
-async deleteOK() {
-    this.showSpinner();
-    this.deleteDialog.close();
-    var result = await this.repository.delete(this.record.id);
-    this.hideSpinner();
-
-    if (result.error) {
-        this.showErrorMessage(result.description);
+    //-----------------------------------------------------------------------------------------
+    delete() {
+        this.deleteDialog = DialogUtility.confirm({
+            title: 'Delete Vendor',
+            content: `Are you sure you want to delete the vendor <b>${this.record.name}</b>?`,
+            okButton: { click: this.deleteOK.bind(this) }
+        });
     }
-    else {
-        this.showDeleteMessage(true);
-        setTimeout(() => this.router.navigate(['/customer/edit', this.record.customerId]), 1000);
+    //-----------------------------------------------------------------------------------------
+    async deleteOK() {
+        this.showSpinner();
+        this.deleteDialog.close();
+        var result = await this.repository.delete(this.record.id);
+        this.hideSpinner();
+
+        if (result.error) {
+            this.showErrorMessage(result.description);
+        }
+        else {
+            this.showDeleteMessage(true);
+            setTimeout(() => this.router.navigate(['/test/list']), 1000);
+        }
     }
-}
+    //-----------------------------------------------------------------------------------------
 }
