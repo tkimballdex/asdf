7d610e57c9cb55ad3d0f20c6768ed8ffcfa9517e
John Shtir <jshtir@live.com>
2021-01-25 12:25:04 -0800

    implemented buttongroup for nullable boolean

--- ClientApp/src/app/sample/module.ts
+++ ClientApp/src/app/sample/module.ts
@@ -14,6 +14,7 @@ import { DropDownListModule } from '@syncfusion/ej2-angular-dropdowns';
 import { DatePickerModule } from '@syncfusion/ej2-angular-calendars';
 import { TabModule } from '@syncfusion/ej2-angular-navigations';
 import { SwitchModule } from '@syncfusion/ej2-angular-buttons';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 @NgModule({
     declarations: [
@@ -22,7 +23,7 @@ import { SwitchModule } from '@syncfusion/ej2-angular-buttons';
     ],
     imports: [
         GridModule, RadioButtonModule, ButtonModule, TextBoxModule, CheckBoxModule, ToastAllModule, DropDownListModule, 
-        TabModule, DatePickerModule, SwitchModule, NumericTextBoxModule, CommonModule,
+        TabModule, DatePickerModule, SwitchModule, NumericTextBoxModule, MatButtonToggleModule, CommonModule, 
         RouterModule.forChild([
             { path: 'sample/list', component: SampleListComponent, canActivate: [MsalGuard] },
             { path: 'sample/add', component: SampleEditComponent, canActivate: [MsalGuard] },
