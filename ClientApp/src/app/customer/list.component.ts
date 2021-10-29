b010f8f5fb017bc29a9b029ab426596a3011d380
Vlad Sfatcu <vladucu@hotmail.com>
2021-10-29 00:58:37 -0700

    Customer => form state

--- ClientApp/src/app/customer/list.component.ts
+++ ClientApp/src/app/customer/list.component.ts
@@ -3,68 +3,78 @@ import { GridComponent, ExcelExportProperties, ExcelExportService, Column } from
 import { CustomerRepository } from './repository';
 import { PageComponent } from '../shared/page.component';
 import { AppService, EventQueueService, AppEvent, AppEventType } from '../shared/app.service';
+import { GridFormParams, FormState } from '../shared/formState';
 import { TenantService } from '../shared/tenant.service';
 
 @Component({
-    selector: 'customer-list',
-    templateUrl: './list.component.html',
-    styleUrls: ['./list.component.scss']
+	selector: 'customer-list',
+	templateUrl: './list.component.html',
+	styleUrls: ['./list.component.scss']
 })
-export class CustomerListComponent extends PageComponent implements OnInit {  
-       
-	constructor(private repository: CustomerRepository, private appService: AppService, private tenant: TenantService, private eventQueue: EventQueueService) {
-        super();
-    }
-    //------------------------------------------------------------------------------------------------------------------------
-    public list: any;
-    public name: any;
-    @ViewChild('grid') public grid: GridComponent;    
-    //------------------------------------------------------------------------------------------------------------------------
-    async ngOnInit() {
+export class CustomerListComponent extends PageComponent implements OnInit {
+
+	constructor(private repository: CustomerRepository, private appService: AppService, private tenant: TenantService, private eventQueue: EventQueueService, private formState: FormState) {
+		super();
+	}
+	//------------------------------------------------------------------------------------------------------------------------
+	public form: GridFormParams;
+	public list: any;
+	@ViewChild('grid') public grid: GridComponent;
+	//------------------------------------------------------------------------------------------------------------------------
+	async ngOnInit() {
 		this.privileges = (await this.appService.getPrivileges()).customers;
 		await this.tenant.validate();
+		this.formState.setup(this);
+		this.search();
+	}
+	//------------------------------------------------------------------------------------------------------------------------
+	async searchClick() {
+		this.form.resetGrid(this.grid);
+		this.search();
+	}
+	//------------------------------------------------------------------------------------------------------------------------
+	async search() {
+		this.formState.save(this);
+		this.showSpinner();
+		this.list = await this.repository.list({ tenantId: this.tenant.id, name: this.form['name'] });
+		this.hideSpinner();
+	}
+	//----------------------------------------------------------------------------
+	gridActionHandler(e) {
+		this.form.gridAction(this.grid, e);
+		this.formState.save(this);
+	}
+	//------------------------------------------------------------------------------------------------------------------------
+	async export() {
+		this.showSpinner();
 
-        if (this.tenant.id) {
-            this.search();
-        }
-    }
-    //------------------------------------------------------------------------------------------------------------------------
-    async search() {
-        this.showSpinner();
-        this.list = await this.repository.list({ tenantId: this.tenant.id, name: this.name });
-        this.hideSpinner();
-    }
-    //------------------------------------------------------------------------------------------------------------------------
-    async export() {
-        this.showSpinner();
+		(this.grid.columns[0] as Column).visible = false;
+		const excelExportProperties: ExcelExportProperties = {
+			includeHiddenColumn: true,
+			fileName: 'customers.xlsx'
+		};
+		this.grid.excelExport(excelExportProperties);
 
-        (this.grid.columns[0] as Column).visible = false;
-        const excelExportProperties: ExcelExportProperties = {
-            includeHiddenColumn: true,
-            fileName: 'customers.xlsx'
-        };
-        this.grid.excelExport(excelExportProperties);
-
-        this.hideSpinner();
-    }
-    //------------------------------------------------------------------------------------------------------------------------
-    excelExportComplete(): void {
-        (this.grid.columns[0] as Column).visible = true;
-    }  
-    //------------------------------------------------------------------------------------------------------------------------
-    validateEmail(email) {
-        var re = /\S+@\S+\.\S+/;
-        return re.test(email);
-    }
-    //------------------------------------------------------------------------------------------------------------------------
-    sendEmail(): void {
-        var email = this.list.map(x => { return this.validateEmail(x.contactEmail) ? { name: x.contactName, email: x.contactEmail } : null; }).filter(x => x != null);
-        this.eventQueue.dispatch(new AppEvent(AppEventType.SendEmail, email));
-    }
-    //------------------------------------------------------------------------------------------------------------------------
+		this.hideSpinner();
+	}
+	//------------------------------------------------------------------------------------------------------------------------
+	excelExportComplete(): void {
+		(this.grid.columns[0] as Column).visible = true;
+	}
+	//------------------------------------------------------------------------------------------------------------------------
+	validateEmail(email) {
+		var re = /\S+@\S+\.\S+/;
+		return re.test(email);
+	}
+	//------------------------------------------------------------------------------------------------------------------------
+	sendEmail(): void {
+		var email = this.list.map(x => { return this.validateEmail(x.contactEmail) ? { name: x.contactName, email: x.contactEmail } : null; }).filter(x => x != null);
+		this.eventQueue.dispatch(new AppEvent(AppEventType.SendEmail, email));
+	}
+	//------------------------------------------------------------------------------------------------------------------------
 	sendSms(): void {
 		var email = this.list.map(x => { return x.phoneNo ? { name: x.contactName, phoneNo: x.phoneNo } : null; }).filter(x => x != null);
 		this.eventQueue.dispatch(new AppEvent(AppEventType.SendSms, email));
 	}
-    //------------------------------------------------------------------------------------------------------------------------
+	//------------------------------------------------------------------------------------------------------------------------
 }
---
+++ ClientApp/src/app/shared/formState.ts
@@ -0,0 +1,59 @@
+import { Injectable } from "@angular/core";
+import { GridComponent, PageSettingsModel, SortSettingsModel } from '@syncfusion/ej2-angular-grids';
+
+export class GridFormParams {
+	public pageSettings: PageSettingsModel;
+	public sortSettings: SortSettingsModel;
+	public sortSettingsCurrent: SortSettingsModel;
+
+	constructor() {
+		this.pageSettings = { pageSizes: true, pageSize: 10, currentPage: 1 };
+	}
+
+	gridAction(grid: GridComponent, e) {
+		this.pageSettings.currentPage = grid.pagerModule.pagerObj.currentPage;
+
+		if (e.requestType == 'sorting') {
+			this.sortSettingsCurrent = e.columnName ? { columns: [{ field: e.columnName, direction: e.direction }] } : null;
+		}
+		else if (e.requestType = 'paging') {
+			this.pageSettings.currentPage = grid.pagerModule.pagerObj.currentPage;
+			this.pageSettings.pageSize = grid.pagerModule.pagerObj.pageSize;
+		}
+	}
+
+	resetGrid(grid) {
+		this.pageSettings.currentPage = grid.pagerModule.pagerObj.currentPage = 1;
+	}
+}
+
+@Injectable({ providedIn: 'root' })
+export class FormState {
+	public componentState = {};
+	public readonly GuidEmpty = "00000000-0000-0000-0000-000000000000";
+
+	constructor() {
+	}
+
+	public save(page: any) {
+		this.componentState[page.constructor.name] = page.form;
+	}
+
+	public get(page: any) {
+		var form = this.componentState[page.constructor.name];
+
+		if (form && form.sortSettingsCurrent) {
+			form.sortSettings = form.sortSettingsCurrent;
+		}
+
+		return form;
+	}
+
+	public setup(page: any) {
+		page.form = this.get(page);
+
+		if (!page.form || !history.state.formState) {
+			page.form = new GridFormParams();
+		}
+	}
+}
