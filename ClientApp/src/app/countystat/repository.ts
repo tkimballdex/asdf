f3ad04e2a5dd708ff7bd80e30b621b3a88004fba
Vishwa Patel <vishwa.patel@interome.com>
2021-11-16 16:35:15 -0800

    starting on countystat

---
+++ ClientApp/src/app/countystat/repository.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { MsalHttpClient } from '../shared/msal-http';
+
+@Injectable({ providedIn: 'root' })
+export class CountystatRepository {
+    constructor(private http: MsalHttpClient) {
+    }
+
+    public save(record: any) {
+        return this.http.post<any>(`/countystat/save`, record);
+    }
+
+    public delete(id: string) {
+        return this.http.post<any>(`/countystat/delete/${id}`);
+    }
+
+    public get(id: number) {
+        return this.http.post(`/countystat/get/${id}`);
+    }
+
+    public list(filter: any) {
+        return this.http.post(`/countystat/list`, filter);
+    }
+}
